81477fc8d8fd9f9f853bb7c983e2cbb5fee302c9
Lucia Profeta <lucia.profeta@colorado.edu>
2023-11-09 22:31:23 -0700

    populated insert.sql (URL and Passwords Incomplete) and made admin accounts

--- All Project code/src/init_data/insert.sql
+++ All Project code/src/init_data/insert.sql
@@ -7,14 +7,14 @@ INSERT INTO Users(username, name, hashpw, adminID) VALUES
 ('vince', 'Vince', 'password', 6);
 
 INSERT INTO Users(username, name, address, hashpw, photoURL) VALUES
-('Puppies123', 'Mary Smith', '710 Smith Road', 'puppies4life', 'URL'),
-('BunnyLuvr', 'Carter Watts', '12 Bluebell Lane', 'bunnys4life', 'URL'),
-('TooManyCats', 'Abby Greer', '2345 Goldfish Ave', 'kittens4like', 'URL');
+('Puppies123', 'Mary Smith', '710 Smith Road', 'puppies4life', 'http://dummyimage.com/162x100.png/dddddd/000000'),
+('BunnyLuvr', 'Carter Watts', '12 Bluebell Lane', 'bunnys4life', 'http://dummyimage.com/162x100.png/dddddd/000000'),
+('TooManyCats', 'Abby Greer', '2345 Goldfish Ave', 'kittens4like', 'http://dummyimage.com/162x100.png/dddddd/000000');
 
 INSERT INTO PetInfo(name, animalType, breed, size, age, sex, description, adoptionFee, photoURL, username) VALUES
-('Daisy', 1, 'Golden Retriever', 3, 2, 1, 'Daisy loves sunshine and long walks', 250, 'URL', 'Puppies123'),
-('Baxter', 2, 'Korat', 2, 3, 2, 'He is friendly and loves pets. He only eats Tuna', 2500, 'URL', 'lucy'),
-('Carrots', 3, 'Rabbit', 1, 1, 2, 'Carrots loves carrots! He will bite your fingers', 25, 'URL', 'BunnyLuvr');
+('Daisy', 1, 'Golden Retriever', 3, 2, 1, 'Daisy loves sunshine and long walks', 250, 'http://dummyimage.com/162x100.png/dddddd/000000', 'Puppies123'),
+('Baxter', 2, 'Korat', 2, 3, 2, 'He is friendly and loves pets. He only eats Tuna', 2500, 'http://dummyimage.com/162x100.png/dddddd/000000', 'lucy'),
+('Carrots', 3, 'Rabbit', 1, 1, 2, 'Carrots loves carrots! He will bite your fingers', 25, 'http://dummyimage.com/162x100.png/dddddd/000000', 'BunnyLuvr');
 
 INSERT INTO Applications(status, username, petID) VALUES
 (1,'TooManyCats', 2);
