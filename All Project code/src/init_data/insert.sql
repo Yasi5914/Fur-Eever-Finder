e4c65a10e5147b20b460079343b9d15fc2e7b9d7
Lucia-Profeta <lucia.profeta@colorado.edu>
2023-11-30 17:55:14 -0700

    photos display on explore

--- All Project code/src/init_data/insert.sql
+++ All Project code/src/init_data/insert.sql
@@ -3,9 +3,9 @@
 -- password: testing
 
 INSERT INTO PetInfo(name, animalType, breed, size, age, sex, description, adoptionFee, petPhoto) VALUES
-('Daisy', 1, 'Golden Retriever', 1, 1, 1, 'Daisy loves to play fetch and lay in the sunshine. She is scared of thunder and needs lots of pets when there is a storm.', 100, 'https://github.com/Yasi5914/Final_project_3308_team_1/blob/updateStarterPets/All%20Project%20code/src/resources/img/dog.jpeg?raw=true'),
-('Max', 2, 'Maine Coon', 2, 11, 2, 'He is friendly and loves pets. Max does not like kids.', 75, 'https://github.com/Yasi5914/Final_project_3308_team_1/blob/updateStarterPets/All%20Project%20code/src/resources/img/cat.jpeg?raw=true'),
-('Carrots', 3, 'Bunny', 1, 4, 2, 'Carrots loves carrots! Be careful because he will bite your fingers.', 40, 'http://dummyimage.com/162x100.png/dddddd/000000');
+('Daisy', 1, 'Golden Retriever', 1, 1, 1, 'Daisy loves to play fetch and lay in the sunshine. She is scared of thunder and needs lots of pets when there is a storm.', 100, 'https://github.com/Yasi5914/Final_project_3308_team_1/blob/petpic/All%20Project%20code/src/resources/img/dog.jpeg?raw=true'),
+('Max', 2, 'Maine Coon', 2, 11, 2, 'He is friendly and loves pets. Max does not like kids.', 75, 'https://github.com/Yasi5914/Final_project_3308_team_1/blob/petpic/All%20Project%20code/src/resources/img/cat.jpeg?raw=true'),
+('Carrots', 3, 'Bunny', 1, 4, 2, 'Carrots loves carrots! Be careful because he will bite your fingers.', 40, 'https://github.com/Yasi5914/Final_project_3308_team_1/blob/petpic/All%20Project%20code/src/resources/img/other.jpeg?raw=true');
 
 -- INSERT INTO Users(username, name, hashPW, adminID) VALUES
 -- ('lucy', 'Lucy', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true),
@@ -14,7 +14,6 @@ INSERT INTO PetInfo(name, animalType, breed, size, age, sex, description, adopti
 -- ('matt', 'Matt', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true),
 -- ('andrew', 'Andrew', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true),
 -- ('vince', 'Vince', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true);
-
 -- INSERT INTO Users(username, name, address, hashPW, photoURL) VALUES
 -- ('Puppies123', 'Mary Smith', '710 Smith Road', 'puppies4life', 'http://dummyimage.com/162x100.png/dddddd/000000'),
 -- ('BunnyLuvr', 'Carter Watts', '12 Bluebell Lane', 'bunnys4life', 'http://dummyimage.com/162x100.png/dddddd/000000'),
