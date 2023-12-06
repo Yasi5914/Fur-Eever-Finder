e16bb8b34f02414e0b48c41ad8fadf8d75b95906
Yasi5914 <123127302+Yasi5914@users.noreply.github.com>
2023-12-05 18:16:30 -0700

    added pets and button to petpage

--- All Project code/src/init_data/insert.sql
+++ All Project code/src/init_data/insert.sql
@@ -5,24 +5,10 @@
 INSERT INTO PetInfo(name, animalType, breed, size, age, sex, description, adoptionFee, petPhoto) VALUES
 ('Daisy', 1, 'Golden Retriever', 1, 1, 1, 'Daisy loves to play fetch and lay in the sunshine. She is scared of thunder and needs lots of pets when there is a storm.', 100, 'https://www.windyknollgoldens.com/wp-content/uploads/2017/06/Sweet-gentle-and-adorbale-AKC-GOlden-Retriever-puppies-are-ideal-for-the-family.jpg'),
 ('Max', 2, 'Maine Coon', 2, 11, 2, 'He is friendly and loves pets. Max does not like kids.', 75, 'https://th.bing.com/th/id/OIP.oRaH-njWnwwTsLG546BangHaFj?rs=1&pid=ImgDetMain'),
-('Carrots', 3, 'Bunny', 1, 4, 2, 'Carrots loves carrots! Be careful because he will bite your fingers.', 40, 'https://th.bing.com/th/id/R.fb0b44a264f4cd932aacfa4a424bbcd7?rik=Tp4%2f64G95aCYmQ&pid=ImgRaw&r=0');
-
--- INSERT INTO Users(username, name, hashPW, adminID) VALUES
--- ('lucy', 'Lucy', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true),
--- ('lindy', 'Lindy', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true),
--- ('yash', 'Yash', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true),
--- ('matt', 'Matt', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true),
--- ('andrew', 'Andrew', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true),
--- ('vince', 'Vince', '$2b$10$iSW9GnHr.8YQyc5unfskdeF2EI/pj/GJhqVP45pkrHfzcJhxG3qPa', true);
--- INSERT INTO Users(username, name, address, hashPW, photoURL) VALUES
--- ('Puppies123', 'Mary Smith', '710 Smith Road', 'puppies4life', 'http://dummyimage.com/162x100.png/dddddd/000000'),
--- ('BunnyLuvr', 'Carter Watts', '12 Bluebell Lane', 'bunnys4life', 'http://dummyimage.com/162x100.png/dddddd/000000'),
--- ('TooManyCats', 'Abby Greer', '2345 Goldfish Ave', 'kittens4like', 'http://dummyimage.com/162x100.png/dddddd/000000');
-
--- INSERT INTO Applications(status, username, petID) VALUES
--- (1,'TooManyCats', 2);
-
--- INSERT INTO UserFavorites(username, petID) VALUES
--- ('puppies123', 1),
--- ('puppies123', 2),
--- ('TooManyCats', 3);
+('Baxter', 3, 'Maine Coon', 3, 4, 1, 'He cute and loves people', 2, 'https://images.unsplash.com/photo-1510771463146-e89e6e86560e?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+('Matt', 5, 'Corgi', 4, 6, 2, 'He Loves Anime', .1, 'https://images.unsplash.com/photo-1458724338480-79bc7a8352e4?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+('Gatito', 6, 'cat', 4, 12, 2, 'He is Lazy', 57, 'https://images.unsplash.com/photo-1596921825946-d738194fac80?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+('Rojo', 7, 'domestic shorthair', 4, 7, 2, 'He Pulls hella chicas buenasa', 97, 'https://images.unsplash.com/photo-1573865526739-10659fec78a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D'),
+('Red', 8, 'domestic shorthair', 4, 6, 2, 'Red is a charming and playful feline with vibrant ginger fur, whose mischievous antics and affectionate purrs make them a delightful companion.', 84765, 'https://images.unsplash.com/photo-1546459094-0b7758695a4b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+('Wanga', 9, 'domestic shorthair', 4, 7, 2, 'Wanga is an enigmatic and sleek cat, adorned with a glossy black coat and piercing yellow eyes, exuding an air of mystery and elegance as they gracefully navigate their surroundings.', 7685, 'https://images.unsplash.com/photo-1550414485-9f22b971dbf0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+    ('Carrots', 10, 'Bunny', 1, 4, 2, 'Carrots loves carrots! Be careful because he will bite your fingers.', 40, 'https://th.bing.com/th/id/R.fb0b44a264f4cd932aacfa4a424bbcd7?rik=Tp4%2f64G95aCYmQ&pid=ImgRaw&r=0');
